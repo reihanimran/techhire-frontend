b1338c45c07d2bd8c9b2af379a268c01da807d9f
Reihan Imran <reihanimran123@gmail.com>
2024-10-26 20:44:58 +0530

    feat:update lib 2

--- src/lib/services/api/company.js
+++ src/lib/services/api/company.js
@@ -1,14 +1,14 @@
 export const createCompany = async ({company}) => {
     const token = await window.Clerk.session.getToken();
   
-    await fetch("http://localhost:8000/company", {
+    await fetch("https://techhire-reihanimran.netlify.app/company", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${token}`,
         },
         body: JSON.stringify({
-        job
+        company
       }),
     });
 }
@@ -16,7 +16,7 @@ export const createCompany = async ({company}) => {
 export const getCompanyById = async ({id}) => {
   const token = await window.Clerk.session.getToken();
 
-  await fetch(`http://localhost:8000/company/${id}`, {
+  await fetch(`https://techhire-reihanimran.netlify.app/company/${id}`, {
       method: "GET",
       headers: {
         "Authorization":`Bearer ${token}`,
